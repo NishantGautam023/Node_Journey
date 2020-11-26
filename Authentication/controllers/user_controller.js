64fdf8fe34f1e4465345dc9d4793548a37a585d3
Nishant Gautam <eddienishant@gmail.com>
2020-11-26 17:03:40 +0530

    added user sign-up form

--- Authentication/controllers/user_controller.js
+++ Authentication/controllers/user_controller.js
@@ -3,3 +3,19 @@ module.exports.profile = function(req,res) {
         title: "User Profile"
     })
 }
+
+// Adding a SignIn action and rendering in to the Sign up page
+
+module.exports.signIn = function(req,res) {
+    res.render('user_sign_in', {
+        title: "Sign In "
+    })
+}
+
+// Adding a SignIn action and rendering in to the Sign up page
+
+module.exports.signUp = function(req,res) {
+    res.render('user_sign_up', {
+        title: "Sign Up "
+    })
+}

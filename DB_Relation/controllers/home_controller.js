bf7ec25066426b07f1f54b1e6d7acf1467d763ff
Nishant Gautam <eddienishant@gmail.com>
2020-12-04 11:51:39 +0530

    display post and realted user

--- DB_Relation/controllers/home_controller.js
+++ DB_Relation/controllers/home_controller.js
@@ -2,16 +2,21 @@ const Post = require('../models/posts');
 
 module.exports.home = function(req, res) {
 
- Post.find({}, function(err, posts) {
+//  Post.find({}, function(err, posts) {
 
-     res.render('home', {
-         title: "NOdeial | Home",
+//      res.render('home', {
+//          title: "NOdeial | Home",
+//          posts: posts
+//      })
+//  } )
+
+    // populate the user of each post
+  
+ Post.find({}).populate('user').exec(function(err,posts){
+     return res.render('home',{
+         title: "Nodial | Home",
          posts: posts
-     })
-    
-
-    
- } )
-
+     });
+ })
 
 }

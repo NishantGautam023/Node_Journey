0e082a3238f0b0487b9a24649d78409cb7c33cf0
Nishant Gautam <eddienishant@gmail.com>
2020-12-04 16:19:23 +0530

    checked authentication by creating post

--- DB_Relation/routes/post.js
+++ DB_Relation/routes/post.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const passport = require('passport');
 
 // import the controller to get the action
 const postController = require('../controllers/post_controller')
 // now we can access all the action that are exported in this post_controller.
 
 // Since we put the method = post in the form so
-router.post('/create', postController.create)
+router.post('/create',passport.checkAuthentication, postController.create)
 
 
 // exporting it 
